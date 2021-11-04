978a5b8594d66e64f682b49b4f3ee159b2e6ef25
krislee2020 <liyan@ayyz.edu.cn>
2021-11-04 15:05:50 +0800

    修改

--- hack/session2.php
+++ hack/session2.php
@@ -6,15 +6,15 @@ $pwd = @$_POST['PWD'];
  // 用户名和密码中任意一项未输入时则登录失败
 if ($id == '' || $pwd == '') 
 {
-die(' 登录失败 '); 
+	die(' 登录失败 '); 
  }  
 $_SESSION['ID'] = $id; 
 $_SESSION['PWD'] =$pwd; 
 ?>
 <html>
-<head>
-<title> 登录 </title>
-</head>
-<body> 登录成功 <a href="session3.php"> 我的账号 </a>
- </body>
+	<head>
+		<title> 登录 </title>
+	</head>
+		<body> 登录成功 <a href="session3.php"> 我的账号 </a>
+		</body>
 </html>
